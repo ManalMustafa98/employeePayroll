e8b963bd816d75da7cf5fe10d8a428fd13ba9a89
HAMMAD <hammadasif039@gmail.com>
2025-05-31 02:06:34 +0500

    done

---
+++ ViewAllEmployeeAdmin.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class ViewAllEmployeeAdmin {
+
+    private JList list1;
+    private JList list2;
+    private JList list3;
+    private JList list4;
+    private JList list5;
+    private JButton button1;
+    private JButton button2;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
