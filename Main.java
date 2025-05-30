27c613f568d4344f137846242bd1422a6ce7b783
Manal <manalpretty284@gmail.com>
2025-05-31 00:56:40 +0800

    ok

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Employee Payroll System");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(800, 600);
+            frame.setSize(700, 500);
             frame.setLocationRelativeTo(null); // Center on screen
             WelcomePage welcomePanel = new WelcomePage(frame);
             frame.setContentPane(welcomePanel.getPanel());
--- ViewAttendanceForAllEmployee_Admin.java
+++ ViewAttendanceForAllEmployee_Admin.java
@@ -1,2 +1,13 @@
+import javax.swing.*;
+
 public class ViewAttendanceForAllEmployee_Admin {
+    private JPanel panel1;
+    private JButton backButton;
+    private JList list1;
+    private JList list2;
+    private JList list3;
+    private JList list4;
+    private JList list5;
+
+
 }
